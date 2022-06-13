d26b76e567760b5152fa631eee2215bb9ddc393e
hereIam-dev <mike.kaden@hereiam.solutions>
2022-06-13 14:56:21 +0200

    update viewport and meta

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const APP_NAME = 'hereIam prototype'
+const APP_DESCRIPTION = 'Saving lives with better decisions'
+
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='en' dir='ltr'>
         <Head>
             <link rel="manifest" href="%PUBLIC_URL%/manifest.webmanifest" />
             <meta charset='utf-8' />
             <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-            <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-            <meta name='description' content='Description' />
-            <meta name='keywords' content='Keywords' />
+            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+            <meta name='application-name' content={APP_NAME} />
+            <meta name='apple-mobile-web-app-capable' content='yes' />
+            <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+            <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+            <meta name='description' content={APP_DESCRIPTION} />
+            <meta name='format-detection' content='telephone=no' />
+            <meta name='mobile-web-app-capable' content='yes' />
             <link href='%PUBLIC_URL%/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />    
             <link href='%PUBLIC_URL%/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
             <link rel='apple-touch-icon' href='%PUBLIC_URL%/icon/apple-icon.png'></link>
