bb2dc921440202ab7d29d9ccc4b6f6f158d3d95f
hereIam-dev <mike.kaden@hereiam.solutions>
2022-06-13 12:59:49 +0200

    fallback an clean dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,10 @@ import styled from 'styled-components';
 const Home = () => {
   return <StyledExample>
   
-  <>
     <Head>
       <title>hereIam - saving lives with better decisions</title>
     </Head>
     <h1>INTRO</h1>
-    <h2>
-      <Link href='/'>Go to Home</Link>
-    </h2>
-    <h2>
-      <Link href='/profile'>Go to route /profile</Link>
-    </h2>
-    <h2>
-      <Link href='/settings'>Go to route /settings</Link>
-    </h2>
-    <h2>
-      <Link href='/about'>Go to route /about</Link>
-    </h2>
-  </>
   
   </StyledExample>;
 };
