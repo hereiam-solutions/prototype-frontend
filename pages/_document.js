51f2978c4341efb8575fb039653c8149b343594a
hereIam-dev <mike.kaden@hereiam.solutions>
2022-06-13 16:17:39 +0200

    fix minimal

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
     return (
       <Html lang='en' dir='ltr'>
         <Head>
-            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+            <link rel="manifest" href="public/manifest.json" />
             <meta charSet='utf-8' />
             <meta httpEquiv='X-UA-Compatible' content='IE=edge' />    
             <meta name='application-name' content={APP_NAME} />
