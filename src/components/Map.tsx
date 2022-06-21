804de3799a6c5ac2472b1d2084cf6ab0c1112610
robinwettstaedt <robinwettstadt@gmail.com>
2022-06-21 12:38:53 +0200

    get leaflet working

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,42 +1,69 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import mockLocationData from '../data/locationData.json';
+
+const fireHazardIcon = new Icon({
+  iconUrl: '/fire_hazard_icon.svg',
+  iconSize: [25, 25],
+});
+
+type Location = [latitude: number, longitude: number];
+
+type ActiveMarkerType = {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+};
+
 const Map = () => {
-  useEffect(() => {
-    const L = require('leaflet');
+  //   const map = useMap();
 
-    delete L.Icon.Default.prototype._getIconUrl;
+  const [currentLocation, setCurrentLocation] = useState<Location>([
+    52.520008, 13.404954,
+  ]);
 
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-      iconUrl: require('leaflet/dist/images/marker-icon.png'),
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-    });
-  }, []);
+  const [activeMarker, setActiveMarker] = useState<ActiveMarkerType | null>(
+    null
+  );
 
   return (
     <>
       <StyledMapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={currentLocation}
+        zoom={12}
         scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+        {mockLocationData.features.map((location) => (
+          <Marker
+            key={location.id}
+            position={[location.latitude, location.longitude]}
+            icon={fireHazardIcon}
+            eventHandlers={{ click: () => setActiveMarker(location) }}
+          />
+        ))}
+        {activeMarker && (
+          <Popup
+            position={[activeMarker.latitude, activeMarker.longitude]}
+            eventHandlers={{ popupclose: () => setActiveMarker(null) }}
+          >
+            {activeMarker.name}
           </Popup>
-        </Marker>
+        )}
       </StyledMapContainer>
     </>
   );
 };
 
+// styled components
 const StyledMapContainer = styled(MapContainer)`
   height: 100vh;
   width: 100vw;
