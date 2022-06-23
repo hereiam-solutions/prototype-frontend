fc7c474667c9a7321da5e15a56b0bbf352a7e974
Mike Kaden <mike.kaden@hereiam.solutions>
2022-06-23 16:15:44 +0200

    add global system font to app

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -12,6 +12,12 @@ export const GlobalStyles = createGlobalStyle`
    *:focus {
     outline: none;
    }
+   html{
+    font-family: -apple-system, BlinkMacSystemFont,
+    "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans",
+    "Droid Sans", "Helvetica Neue", sans-serif;
+  }
 `;
 
 export const globalDarkThemeValues = {};
