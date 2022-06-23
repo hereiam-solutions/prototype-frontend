ff8b0cee25e795bcf7ea4d941cc78397058c7b8a
Mike Kaden <mike.kaden@hereiam.solutions>
2022-06-23 17:18:33 +0200

    inject color variables and resets

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -12,11 +12,71 @@ export const GlobalStyles = createGlobalStyle`
    *:focus {
     outline: none;
    }
-   html{
+
+   :root {
+    --base-light: #DCE4EF;
+    --base: #494440;
+    --base-dark: #212121;
+    --gray-500 #323A45;
+    --gray-400: #5B616B;
+    --gray-300: #AEB0B5;
+    --gray-200: #D6D7D9;
+    --gray-150: #E4E2E0;
+    --gray-100: #F1F1F1;
+    --cta-100: #0071BC;
+    --cta-200: #205493;
+    --cta-300: #112E51;
+    --attention: #F8DE00;
+    --alert: #D2251E;
+    --ready: #4CBA4B;
+    --whiter: #fffff8;
+  }
+
+   *, *::before, *::after {
+    margin: 0;
+    border: 0;
+    padding: 0;
+    -moz-box-sizing: inherit;
+    -webkit-box-sizing: inherit;
+    box-sizing: inherit;
+  }
+
+  html{
     font-family: -apple-system, BlinkMacSystemFont,
     "Segoe UI", "Roboto", "Oxygen",
     "Ubuntu", "Cantarell", "Fira Sans",
     "Droid Sans", "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    color: var(--base-dark);
+  }
+
+  img {
+    border: 0;
+    display: block;
+    max-width: 100%;
+    height: auto;
+    image-rendering: -webkit-optimize-contrast
+  }
+
+  svg:not(:root) {
+    overflow: hidden
+  }
+
+  figure {
+    margin: 0
+  }
+
+  button, html input[type="button"], input[type="reset"], input[type="submit"] {
+    -webkit-appearance: button;
+    cursor: pointer
+  }
+
+  input {
+    line-height: normal
+  }
+
+  textarea {
+    overflow: auto
   }
 `;
 
