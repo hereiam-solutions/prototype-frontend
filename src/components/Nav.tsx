79feca9af1ccc9e52ff2f317ca22bac8164b3082
Mike Kaden <mike.kaden@hereiam.solutions>
2022-06-23 21:06:13 +0200

    Nav styled

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { OverlayNavData } from './NavData';
+
+const OverlayNavMenu = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    height: 3rem;
+    transition: .6s;
+`
+const MenuItems = styled.li`
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    width: 100%;
+    height: 35px;
+    padding: 1rem 0 1.25rem;
+`
+const MenuItemLinks = styled(Link)`
+    display: flex;
+    align-items: center;
+    padding: 0 2rem;
+    font-size: 15px;
+    text-decoration: none;
+    color: var(--base-light);
+
+    &:hover {
+        background-color: var(--gray-400);
+        color: var(--gray-200);
+        font-weight: 400;
+        border-radius: 5px;
+        margin: 0 2rem;
+    }
+`
+
 
 const Nav: React.FunctionComponent = () => {
     return (
