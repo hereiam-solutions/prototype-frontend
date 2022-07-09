b3ed577fdad324d1e982f9563eeca25578141762
Mike Kaden <mike.kaden@hereiam.solutions>
2022-07-09 22:04:18 +0200

    rename

--- src/components/navigation/Company.tsx
+++ src/components/navigation/Company.tsx
@@ -1,4 +1,4 @@
-//import { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import useNavigation from '../../hooks/useNavigation';
@@ -6,39 +6,39 @@ import Draw from './Draw';
 
 import { ReactComponent as LogoHereiam } from '../../assets/Logo/light/hereIam_logo_light256x256.svg';
 
-const Company = () => {
+function Company() {
   const { isDrawOpen, setIsDrawOpen } = useNavigation();
 
   return (
     <>
-      <StyledAboutMenu role="navigation">
-        <StyledAboutButton onClick={() => setIsDrawOpen(true)} to="aboutpage">
+      <StyledCompanyMenu role="navigation">
+        <StyledCompanyButton onClick={() => setIsDrawOpen(true)} to="about">
           <LogoHereiam />
-        </StyledAboutButton>
+        </StyledCompanyButton>
         <div>
-            <h4>hereIam</h4>
+          <h4>hereIam</h4>
         </div>
-      </StyledAboutMenu>
+      </StyledCompanyMenu>
 
       {isDrawOpen && <Draw usedInAuthentication={false} />}
     </>
   );
-};
+}
 
-const StyledAboutMenu = styled.div`
+const StyledCompanyMenu = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1.3rem;
   position: absolute;
   bottom: 5%;
-  right: 3rem;
-  z-index: 4;
+  right: 5rem;
+  z-index: 2;
   pointer-events: auto;
 `;
 
-const StyledAboutButton = styled(Link)`
- 
+const StyledCompanyButton = styled(Link)`
+ color: black;
 `;
 
 export default Company;
