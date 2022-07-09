ca9abe73f3cb294fbd906df50fa46209c5504ee7
Mike Kaden <mike.kaden@hereiam.solutions>
2022-07-09 22:04:42 +0200

    fix

--- src/components/navigation/Company.tsx
+++ src/components/navigation/Company.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import useNavigation from '../../hooks/useNavigation';
