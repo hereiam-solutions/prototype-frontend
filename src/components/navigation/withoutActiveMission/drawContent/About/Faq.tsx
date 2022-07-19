78b35442512e3d533d7b1433925c79947387f1e0
Mike Kaden <mike.kaden@hereiam.solutions>
2022-07-19 13:29:00 +0200

    fix

---
+++ src/components/navigation/withoutActiveMission/drawContent/About/Faq.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+
+};
+
+const Faq = (props: Props) => {
+  return (
+    <StyledFaq>
+
+
+            
+    </StyledFaq>
+  )  
+};
+
+const StyledFaq = styled.div`
+    width: 100%;
+    margin: 0.4rem;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+
+export default Faq;
