ddc2bfe102be5b48d9e96bbe280dae09a82426b6
Mike Kaden <mike.kaden@hereiam.solutions>
2022-07-24 10:18:13 +0200

    minimal

--- src/components/navigation/withoutActiveMission/drawContent/Profile/ProfileName.tsx
+++ src/components/navigation/withoutActiveMission/drawContent/Profile/ProfileName.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 import useRealm from '../../../../../hooks/useRealm';
 import styled from 'styled-components';
 
 const ProfileName = () => {
+
   return (
     <StyledNameWrapper>
+
+      
 
     </StyledNameWrapper>
   );
 };
 
 const StyledNameWrapper = styled.div`
-  position: relative;
+  margin: 0;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  pointer-events: auto;
 `;
 
 export default ProfileName;
