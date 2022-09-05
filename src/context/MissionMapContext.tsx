90afbdbebaacc404f09a1e0fed7094e0c8dbe9e7
Mike Kaden <mike.kaden@hereiam.solutions>
2022-09-05 16:06:51 +0200

    change Sattelite Map to arcgis to avoid google cookies

--- src/context/MissionMapContext.tsx
+++ src/context/MissionMapContext.tsx
@@ -7,11 +7,11 @@ import { ReactNode, createContext, useState } from "react";
 
 export enum ActiveTileLayerEnum {
   DEFAULT = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  //   CYCLOSM = "https://{s}.tile-cyclosm.openstreetmap.fr/[cyclosm|cyclosm-lite]/{z}/{x}/{y}.png",
   HUMANITARIAN = "https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-  SATELLITE = "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+  SATELLITE = "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   // https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}
   // https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/256/{{z}}/{{x}}/{{y}}@2x?access_token={process.env.HEREIAM_MAPBOX_TOKEN}
+  //   CYCLOSM = "https://{s}.tile-cyclosm.openstreetmap.fr/[cyclosm|cyclosm-lite]/{z}/{x}/{y}.png",
 }
 
 // create a type for the context's value
