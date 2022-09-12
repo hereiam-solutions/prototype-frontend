27e2bc912e71e046527a91cf75461434115a51b5
Mike Kaden <mike.kaden@hereiam.solutions>
2022-09-12 15:40:58 +0200

    Changing Icons

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -4,10 +4,18 @@ import useNavigation from "../../hooks/useNavigation";
 import Draw from "./Draw";
 
 // svg imports
-import { ReactComponent as DashboardButton } from "../../assets/Navigation/Dashboard_Dark.svg";
-import { ReactComponent as SettingsButton } from "../../assets/Navigation/Settings.svg";
-import { ReactComponent as ProfileButton } from "../../assets/Navigation/User.svg";
+import { RiFocus2Fill } from "react-icons/ri";
+import { RiSettings4Fill } from "react-icons/ri";
+import { RiTShirtFill } from "react-icons/ri";
+
+//import { ReactComponent as DashboardButton } from "../../assets/Navigation/Dashboard_Dark.svg";
+//import { ReactComponent as SettingsButton } from "../../assets/Navigation/Settings.svg";
+//import { ReactComponent as ProfileButton } from "../../assets/Navigation/User.svg";
 import useMission from "../../hooks/useMission";
+
+let DashboardButton = RiFocus2Fill;
+let SettingsButton = RiSettings4Fill;
+let ProfileButton = RiTShirtFill;
 
 const Nav = () => {
   const { isDrawOpen, setIsDrawOpen } = useNavigation();
@@ -23,7 +31,10 @@ const Nav = () => {
           }}
           to={activeMission ? "dashboard" : "join-mission"}
         >
-          <DashboardButton />
+          <StyledDashBoardButton>
+            <DashboardButton />
+          </StyledDashBoardButton>
+          
         </StyledMenuButton>
 
         <StyledMenuButton
@@ -33,7 +44,10 @@ const Nav = () => {
           }}
           to="settings"
         >
-          <SettingsButton />
+          <StyledSettingsButton>
+            <SettingsButton />
+          </StyledSettingsButton>
+          
         </StyledMenuButton>
 
         <StyledMenuButton
@@ -43,7 +57,10 @@ const Nav = () => {
           }}
           to="profile"
         >
-          <ProfileButton />
+          <StyledProfileButton>
+            <ProfileButton />
+          </StyledProfileButton>
+          
         </StyledMenuButton>
       </StyledNavigationMenu>
 
@@ -65,5 +82,23 @@ const StyledNavigationMenu = styled.div`
 `;
 
 const StyledMenuButton = styled(Link)``;
+
+const StyledDashBoardButton = styled.div`
+  font-size: 3rem;
+  color: ${(props) => props.theme.iconColor};
+  filter: drop-shadow(10px 10px 20px #fffff8) invert(35%);
+`;
+
+const StyledSettingsButton = styled.div`
+  font-size: 3rem;
+  color: ${(props) => props.theme.iconColor};
+  filter: drop-shadow(10px 10px 20px #fffff8) invert(35%);
+`;
+
+const StyledProfileButton = styled.div`
+  font-size: 3rem;
+  color: ${(props) => props.theme.iconColor};
+  filter: drop-shadow(10px 10px 20px #fffff8) invert(35%);
+`;
 
 export default Nav;
