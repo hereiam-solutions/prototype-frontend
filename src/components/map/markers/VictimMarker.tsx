57b0bef15ec8db1d59a20d69380d8b0e54f23e8a
Mike Kaden <mike.kaden@hereiam.solutions>
2022-11-21 20:09:58 +0100

    add functions and update design

--- src/components/map/markers/VictimMarker.tsx
+++ src/components/map/markers/VictimMarker.tsx
@@ -13,10 +13,9 @@ import {
   StyledPopupContentWrapper,
   StyledPopupHeading,
   StyledDate,
-  StyledSection,
   StyledBoldText,
-  StyledStatusText,
   StyledDeactivateButton,
+  StyledSection,
 } from "./styles/markerStyles";
 
 //divlogo for leaflet
@@ -29,10 +28,22 @@ type Props = {
   victim: VictimSchema;
 };
 
+//define victim shortsign
+
+    // get current users firstName and lastName, take the first letter and combine to a capitalized two part abbreviation like "MK"
+
+    // set victimcounter to "0" for current user when createMission
+    // store victimcounter in localStorage
+    // for each victim increment the victimcounter +1
+
+    // combine userShortSign + victimCounter to victimShortSign
+
 //customize logo
+
+//change background-color depends on victim.status
 let iconSize = 30;
 const StyledLocationMarker = styled.button`
- background-color: var(--base-light);
+ background-color: ${(props) => props.theme.alertColor};
  border-radius: 50px;
 `;
 const iconMarker = renderToStaticMarkup(
@@ -51,7 +62,7 @@ const VictimIcon = divIcon({
   iconAnchor: [0, 0],
 });
 
-const PatientMarker = ({ victim }: Props) => {
+const VictimMarker = ({ victim }: Props) => {
   const { realm } = useRealm();
   const { activeMission } = useMission();
   const { reRenderBoolean, setReRenderBoolean } = useMissionMap();
@@ -86,28 +97,62 @@ const PatientMarker = ({ victim }: Props) => {
         <StyledPopupContentWrapper>
           
           <StyledPopupHeading>Victim</StyledPopupHeading>
+          
+          <StyledSection>
+            <StyledShortSign>
+              <StyledVictimID>
+                MK014
+              </StyledVictimID>
+              <StyledHint>
+                Write this ID clearly <br />
+                readable on the victim.
+              </StyledHint>
+            </StyledShortSign>
+          </StyledSection>
 
-          <StyledStatusText>{victim.status}</StyledStatusText>
+          {/*change background-color and color depends victim.status */}
+          <StyledSection>
+            <StyledStatus>{victim.status}</StyledStatus>    
+          </StyledSection>
 
-
-          <StyledSection>     
+          <StyledDataSection>
             <StyledBoldText>{victim.agegroup} years</StyledBoldText>
             <StyledBoldText>{victim.gender}</StyledBoldText>
-          </StyledSection>
+          </StyledDataSection>
 
           <StyledSection>
-            <h4>Extriction</h4>
-            <StyledBoldText>from {victim.total_extrication_from}</StyledBoldText>
-            <StyledBoldText>to {victim.total_extrication_to}</StyledBoldText>
-            <StyledBoldText>at {victim.extricatedLevel}</StyledBoldText>
-            <StyledBoldText>{"->"}{victim.foundStreetAddress}</StyledBoldText>
-            <StyledBoldText>in {victim.positionInStructure}</StyledBoldText>
+            <StyledActionSection>
+              <h4>Actions</h4>
+              <StyledList>
+                {victim.myAction.map(
+                  (action: string, index: number) => {
+                    return (
+                      <StyledListEntry key={index}>{action}</StyledListEntry>
+                    );
+                  }
+                )}
+              </StyledList>
+            </StyledActionSection>
           </StyledSection>
-
+            
           <StyledSection>
-            <h4>Handover</h4>
-            <StyledBoldText>on {victim.handover}</StyledBoldText>
-            <StyledBoldText>name {victim.handoverTo}</StyledBoldText>
+            <StyledExtrictionSection>
+              <h4>Extriction</h4>
+              <StyledBoldText>from {new Date(victim.total_extrication_from).toLocaleString()}</StyledBoldText>
+              <StyledBoldText>to {new Date(victim.total_extrication_to).toLocaleString()}</StyledBoldText>
+              <br />
+              <StyledBoldText>at {victim.extricatedLevel}</StyledBoldText>
+              <StyledBoldText>{"->"}{victim.foundStreetAddress}</StyledBoldText>
+              <StyledBoldText>in {victim.positionInStructure}</StyledBoldText>
+            </StyledExtrictionSection>
+          </StyledSection>
+          
+          <StyledSection>
+            <StyledHandoverSection>
+              <h4>Handover</h4>
+              <StyledBoldText>on {victim.handover}</StyledBoldText>
+              <StyledBoldText>name {victim.handoverTo}</StyledBoldText>
+            </StyledHandoverSection>
           </StyledSection>
 
           <StyledDeactivateButton onClick={deleteVictim}>
@@ -124,4 +169,106 @@ const PatientMarker = ({ victim }: Props) => {
   );
 };
 
-export default PatientMarker;
+const StyledStatus = styled.div`
+
+  width: 100%;
+  self-align: center;
+  padding: 0.5rem;
+
+  color: ${(props) => props.theme.buttonFontColor};
+  font-size: 1.3rem;
+  font-weight: 500;
+  text-align: center;
+
+  border: 1px solid ${(props) => props.theme.buttonFontColor};
+  border-radius: ${(props) => props.theme.primaryBorderRadius};
+
+`;
+
+const StyledShortSign = styled.div`
+
+  width: 100%;
+  padding: 0.5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid ${(props) => props.theme.buttonFontColor};
+  border-radius: ${(props) => props.theme.primaryBorderRadius};
+
+`;
+
+const StyledHint = styled.div`
+  margin-top: 0.3rem;
+
+  font-size: 0.8rem;
+  font-weight: 300;
+  text-align: center;
+`;
+
+const StyledVictimID = styled.div`
+  font-size: 1.3rem;
+  font-weight: 500;
+`;
+
+const StyledDataSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledActionSection = styled.div`
+  margin-top: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledExtrictionSection = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledHandoverSection = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledList = styled.ul`
+  margin: 0.5rem 0 0 0;
+  width: 100%;
+
+  color: ${(props) => props.theme.primaryFontColor};
+`;
+
+const StyledListEntry = styled.li`
+  margin: 0.1rem;
+  padding: 0.2rem 0.6rem;  
+  
+  color: ${(props) => props.theme.buttonFontColor};
+  background-color: ${(props) => props.theme.buttonColor};
+
+  font-weight: 500;
+
+  list-style: none;
+  display: inline-block;
+
+  border-radius: 5px;
+`;
+
+export default VictimMarker;
--- src/components/map/markers/styles/markerStyles.ts
+++ src/components/map/markers/styles/markerStyles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const StyledPopupContentWrapper = styled.div`
+    
   display: flex;
   flex-direction: column;
   justify-content: center;
